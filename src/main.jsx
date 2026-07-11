5ca5f831ae01d0a97bf232c82229c42b703a8790
turkmensah58 <turkmensah58@gmail.com>
2026-07-11 13:41:37 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ document.querySelector("#app").innerHTML = `
  <section class="choose-section">
 
   <div class="choose-heading">
-   <p class="choose-eyebrow">WELCOME TO DΛGDROØM</p>
+   <p class="choose-eyebrow">WELCOME TO D<span class="lambda-fix">Λ</span>GDROØM</p>
 
     <h1>Choose your world</h1>
 
